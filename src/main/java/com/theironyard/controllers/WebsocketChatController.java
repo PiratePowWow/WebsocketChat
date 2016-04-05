b30730ffc4dc17ede293948f64ab699ce78494db
PiratePowWow <Thi3v3ry>
2016-04-04 22:44:13 -0400

    Websocket Chat Add Create Player - Return ArrayList Of Players

--- src/main/java/com/theironyard/controllers/WebsocketChatController.java
+++ src/main/java/com/theironyard/controllers/WebsocketChatController.java
@@ -2,17 +2,29 @@ package com.theironyard.controllers;
 
 import com.theironyard.pojos.Greeting;
 import com.theironyard.pojos.HelloMessage;
+import com.theironyard.pojos.Player;
 import com.theironyard.services.IronMessageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
+import java.util.ArrayList;
+
 /**
  * Created by PiratePowWow on 4/3/16.
  */
 @Controller
 public class WebsocketChatController {
+
+    ArrayList<Player> players = new ArrayList<Player>();
+
+    @MessageMapping("/scoreboard")
+    @SendTo("/topic/scoreboard")
+    public ArrayList<Player> scoreboard(Player player){
+        players.add(player);
+        return new ArrayList<Player>(players);
+    }
 
     @MessageMapping("/hello")
     @SendTo("/topic/greetings")
